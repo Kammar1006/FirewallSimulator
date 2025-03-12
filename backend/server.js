38e347b317538e32499dad8b0d49b72d9abbd70e
Kammar1006 <gim18.kamil.marczynski@gmail.com>
2025-03-12 20:14:15 +0100

    init commit

---
+++ backend/server.js
@@ -0,0 +1,74 @@
+/*
+    Created by Kammar1006
+*/
+
+const PORT = 5003;
+const COOKIE_FLAG = "example_cookie"
+
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+app.use(express.static(`${__dirname}/../frontend`));
+
+const server = http.createServer(app);
+const io = socketio(server, {
+	cookie: true,
+});
+
+const { randomBytes } = require('crypto');
+
+let translationTab = [];
+const setCID = (sock) => {
+	//console.log(sock.request.headers.cookie);
+	let cid;
+	if(sock.handshake.headers.cookie == undefined){
+		return false;
+	}
+	let cookie = (sock.handshake.headers.cookie).split(" ");
+	cookie.forEach(element => {
+		if(element.split("=")[0]==COOKIE_FLAG) cid = element.split("=")[1];	
+	});
+	if(cid == undefined || cid == ""){
+		return false;
+	}
+	else{
+		if(cid[cid.length-1] == ";"){
+			cid = cid.substring(0, cid.length-1);
+		}
+	}
+	return cid;
+}
+const setTranslationTab = (cid) => {
+	if(translationTab[cid]==undefined){
+		translationTab[cid]={
+			user_id: -1,
+			db_stats: {},
+			test_counter: 0,
+		};
+	}
+}
+
+io.on('connection', (sock) => {
+	let cid = setCID(sock);
+	if(!cid){
+		cid = randomBytes(30).toString('hex');
+		sock.emit("set-cookie", `${COOKIE_FLAG}=${cid}; Path=/; SameSite=None; Secure`);
+	}
+	setTranslationTab(cid);
+
+	console.log("User: "+cid);
+
+	sock.on("counter", () => {
+
+		translationTab[cid].test_counter++;
+		sock.emit("message", "Count: "+translationTab[cid].test_counter);
+	});
+
+});
+
+server.listen(PORT, () => {
+	console.log("Work");
+	
+});
