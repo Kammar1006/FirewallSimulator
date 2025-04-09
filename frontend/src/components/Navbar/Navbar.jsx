410156b87c4837b21759ba901a883af654345364
Kuba <272264@student.pwr.edu.pl>
2025-04-09 10:26:45 +0200

    Added login by student id and surname

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import "./navbar.css";
 
 import assets from '../../assets/assets';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div className="navbar">
 
@@ -22,26 +24,25 @@ const Navbar = () => {
             </NavLink>
 
             {/* Right Part */}
-            <div className="navbarContainerRight">
-                <div className="navbarContainerRightContainer">
-                    {/* <NavLink to="/dashboard" className="navbarContainerRightContainerButtonOne">
-                        <button className="navbarContainerRightContainerButtonOneBtn">Dashboard</button>
-                    </NavLink> */}
-                    <div className="navbarContainerRightContainerButtonTwo">
-                        <button className="navbarContainerRightContainerButtonTwoBtn">Settings</button>
+            {location.pathname !== '/login' && (
+                <div className="navbarContainerRight">
+                    <div className="navbarContainerRightContainer">
+                        <div className="navbarContainerRightContainerButtonTwo">
+                            <button className="navbarContainerRightContainerButtonTwoBtn">Settings</button>
+                        </div>
+
+                        <NavLink to="/tasks" className="navbarContainerRightContainerButtonTasks">
+                            <button className="navbarContainerRightContainerButtonTasksBtn">Tasks</button>
+                        </NavLink>
+
+                        <NavLink to="/login" className="navbarContainerRightContainerButtonThree">
+                            <button className="navbarContainerRightContainerButtonThreeBtn glow-on-hover">
+                                <span>Logout</span>
+                            </button>
+                        </NavLink>
                     </div>
-
-                    <NavLink to="/tasks" className="navbarContainerRightContainerButtonTasks">
-                        <button className="navbarContainerRightContainerButtonTasksBtn">Tasks</button>
-                    </NavLink>
-
-                    <NavLink to="/login" className="navbarContainerRightContainerButtonThree">
-                        <button className="navbarContainerRightContainerButtonThreeBtn glow-on-hover">
-                            <span>Logout</span>
-                        </button>
-                    </NavLink>
                 </div>
-            </div>
+            )}
 
         </div>
         
@@ -49,4 +50,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
